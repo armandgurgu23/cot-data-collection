d5e0aac161bf4a4450e77d4a52ec8c9d4f4c97d5
Armand Silviu Gurgu <armandgurgu23@gmail.com>
2024-03-10 17:51:45 -0400

    Finished fetching and dumping data to csv.

---
+++ src/data_collection/cot_data_reader.py
@@ -0,0 +1,33 @@
+
+import requests
+import os
+from io import StringIO
+import pandas as pd
+
+
+class COT_Data_Reader():
+    def __init__(self, dataset_name: str = "short-term-rentals-registration") -> None:
+        self.dataset_name = dataset_name
+        self.base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
+        self.package_endpoint = os.path.join(
+            self.base_url, "api/3/action/package_show")
+        self.package_endpoint_params = {"id": dataset_name}
+        self.resource_prefix = os.path.join(self.base_url, "datastore/dump/")
+
+    def __call__(self):
+        package = requests.get(self.package_endpoint,
+                               params=self.package_endpoint_params).json()
+
+        resource_info = [
+            resource for resource in package['result']['resources']
+            if (resource["datastore_active"]) and (resource['format'] == 'CSV')
+        ][0]
+
+        data_last_updated = resource_info['datastore_cache_last_update']
+        data_url = resource_info['url']
+
+        raw_data = StringIO(requests.get(data_url).text)
+
+        raw_data_df = pd.read_csv(raw_data)
+
+        breakpoint()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import typer
+from src.data_collection.cot_data_reader import COT_Data_Reader
 
 
 def main(dataset_name: str):
-    print(f"Hello {dataset_name}")
+
+    data_fetcher = COT_Data_Reader(dataset_name=dataset_name)
+    data_fetcher()
 
 
 if __name__ == "__main__":
